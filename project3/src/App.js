692bf44e29e5b781729ffab72e506be2d9501010
Krishna Tandon <krishnatandon.1208@gmail.com>
2021-01-21 05:15:10 +0530

    state related proj

---
+++ project3/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+	const [ counter, setCounter ] = useState(0);
+	function incrementCounter() {
+		setCounter(counter + 1);
+		const myid = document.getElementById('xyz');
+		myid.style.display = 'none';
+	}
+
+	// Similar to componentDidMount and componentDidUpdate:
+	// useEffect(() => {
+	// 	// Update the document title using the browser API
+	// 	document.title = `You clicked ${counter} times`;
+	// 	document.body.style.backgroundColor = '#f00';
+	// });
+
+	return (
+		<React.Fragment>
+			<div id="xyz">My ID Data</div>
+
+			<div className="App">
+				{counter}
+
+				<button onClick={incrementCounter}>Click Me </button>
+			</div>
+		</React.Fragment>
+	);
+}
+
+export default App;
